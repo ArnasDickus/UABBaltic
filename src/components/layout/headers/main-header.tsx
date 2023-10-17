ed6fde7f59f54770fa823c57dcbb5f6631f5c2a6
ArnasDickus <arnasdickus@protonmail.com>
2023-10-17 14:36:07 +0300

    fixed header bug

--- src/components/layout/headers/main-header.tsx
+++ src/components/layout/headers/main-header.tsx
@@ -37,108 +37,44 @@ const MainHeader = ({ language, leftNavItems, rightNavItems }: IMainHeader) => {
                 }}
               />
             </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <Link
-                href={`/${language}`}
-                className="flex flex-shrink-0 items-center">
-                <LogoIcon />
-              </Link>
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  {leftNavItems.map((item) => (
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <Link
+                  href={`/${language}`}
+                  className="flex flex-shrink-0 items-center">
+                  <LogoIcon />
+                </Link>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {leftNavItems.map((item) => (
+                      <Link
+                        key={item.title}
+                        // @ts-ignore
+                        href={item.link}
+                        className={` ${isRouteActive(
+                          item.link
+                        )}${linkClassNames}`}
+                        aria-current="page">
+                        {item.title}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="sm:flex hidden space-x-4">
+                  {rightNavItems.map((item) => (
                     <Link
                       key={item.title}
-                      // @ts-ignore
-                      href={item.link}
-                      className={` ${isRouteActive(
+                      className={`${isRouteActive(
                         item.link
-                      )}${linkClassNames}`}
-                      aria-current="page">
+                      )} ${linkClassNames}`}
+                      /* @ts-ignore */
+                      href={item.link}>
                       {item.title}
                     </Link>
                   ))}
                 </div>
-              </div>
-            </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              {/* 
-              // TODO Implement Notification.
-              <button
-                type="button"
-                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span className="absolute -inset-1.5"></span>
-                <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
-              </button> */}
-
-              {/* Display on login */}
-              {/* <div className="relative ml-3">
-                <div>
-                  <button
-                    onClick={() => setDropdownMenu(!dropdownMenu)}
-                    type="button"
-                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    id="user-menu-button"
-                    aria-expanded="false"
-                    aria-haspopup="true">
-                    <span className="absolute -inset-1.5"></span>
-                    <span className="sr-only">Open user menu</span>
-                    <AccountCircleIcon color="warning" fontSize="medium" />
-                  </button>
-                </div>
-
-                {dropdownMenu && (
-                  <div
-                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="user-menu-button">
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      id="user-menu-item-0">
-                      Your Profile
-                    </a>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      id="user-menu-item-1">
-                      Settings
-                    </a>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      id="user-menu-item-2">
-                      Sign out
-                    </a>
-                  </div>
-                )}
-              </div> */}
-              <div className="hidden sm:block">
-                {rightNavItems.map((item) => (
-                  <Link
-                    key={item.title}
-                    className={`${isRouteActive(item.link)} ${linkClassNames}`}
-                    /* @ts-ignore */
-                    href={item.link}>
-                    {item.title}
-                  </Link>
-                ))}
               </div>
             </div>
           </div>
@@ -150,7 +86,7 @@ const MainHeader = ({ language, leftNavItems, rightNavItems }: IMainHeader) => {
                 <Link
                   className={`${isRouteActive(
                     item.link
-                  )}  text-white block rounded-md px-3 py-2 text-base font-medium`}
+                  )} ${linkClassNames} block`}
                   key={item.title}
                   onClick={() => setMenuOpen(false)}
                   // @ts-ignore
