db39dd130f0bd25ac97aaa6127d01bb0509a4818
ArnasDickus <arnasdickus@protonmail.com>
2023-11-09 17:43:22 +0200

    created empty about section

---
+++ src/app/[lng]/portfolio/components/about-section/about-section.tsx
@@ -0,0 +1,10 @@
+import classes from "./about-section.module.css";
+
+const AboutSection = () => {
+  return (
+    <section className={classes.section}>
+      <p className="text-2xl">Hi, I&apos;m Arnas. Nice to meet you</p>
+    </section>
+  );
+};
+export default AboutSection;
--- src/app/[lng]/portfolio/page.tsx
+++ src/app/[lng]/portfolio/page.tsx
@@ -1,15 +1,17 @@
 import { ServerFooter } from "@/components/layout/footer/serverfooter";
-import ExperienceSection from "@/components/portfolio/experience-section/experience-section";
-import HeroSection from "@/components/portfolio/hero-section/hero-section";
-import ProjectsSection from "@/components/portfolio/projects-section/projects-section";
+import ExperienceSection from "@/app/[lng]/portfolio/components/experience-section/experience-section";
+import HeroSection from "@/app/[lng]/portfolio/components/hero-section/hero-section";
+import ProjectsSection from "@/app/[lng]/portfolio/components/projects-section/projects-section";
 
 import { IPageParamProps } from "@/constants/interfaces";
 import PageContainer from "@/styles/components/page-container";
+import AboutSection from "./components/about-section/about-section";
 
 const PagePortfolio = ({ params: { lng } }: IPageParamProps) => {
   return (
     <PageContainer footer={<ServerFooter language={lng} path="/portfolio" />}>
       <HeroSection language={lng} />
+      <AboutSection />
       <ProjectsSection language={lng} />
       <ExperienceSection />
     </PageContainer>
