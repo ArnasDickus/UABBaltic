a7c5d5fbbff30dfaf0754d81f0a52d72282ccf0a
ArnasDickus <arnasdickus@protonmail.com>
2023-11-15 11:04:55 +0200

    build hero-section removed empty pages

--- src/components/layout/footer/footerbase.tsx
+++ src/components/layout/footer/footerbase.tsx
@@ -10,24 +10,9 @@ interface IUseTranslation {
 }
 
 export const FooterBase = async ({ t, lng, path = "" }: IUseTranslation) => {
-  const listItems = [
-    {
-      name: t("about"),
-      link: `/${lng}/about`,
-    },
-    {
-      name: t("privacyPolicy"),
-      link: `/${lng}/privacy-policy`,
-    },
-    {
-      name: t("contact"),
-      link: `/${lng}/contact`,
-    },
-  ];
-
   return (
     <footer>
-      <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <footer className="bg-white rounded-lg shadow mt-4 dark:bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
@@ -37,16 +22,6 @@ export const FooterBase = async ({ t, lng, path = "" }: IUseTranslation) => {
             . {t("allRights")}.
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            {listItems.map((item) => (
-              <li key={item.name}>
-                <Link
-                  className="mr-4 hover:underline md:mr-6 "
-                  /* @ts-ignore */
-                  href={item.link}>
-                  {item.name}
-                </Link>
-              </li>
-            ))}
             {languages
               .filter((language) => lng !== language)
               .map((language, index) => {
