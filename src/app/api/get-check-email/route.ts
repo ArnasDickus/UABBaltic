7ecea6a1f2d7f8981876fcd3147f3bd94a3c833a
ArnasDickus <arnasdickus@protonmail.com>
2023-11-26 17:22:11 +0200

    #33 updated api with additional function

--- src/app/api/get-check-email/route.ts
+++ src/app/api/get-check-email/route.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from "@/constants/status-code";
 import client from "../../../../apollo-client";
 import { GET_USER } from "@/store/modules/user/query";
 import { GetUserQuery, GetUserQueryVariables } from "@/gql/graphql";
-// import { errorResponseHandler } from "@/app/utils/error-response-handler";
+import { errorResponseHandler } from "@/app/utils/error-response-handler";
 import {
   ICheckEmailApi,
   ICheckUsernameRequest,
@@ -13,29 +13,9 @@ interface CustomNextApiRequest extends NextRequest {
   json: () => Promise<NCheckEmail.IRequest["body"]>;
 }
 
-// const checkUserExistence = async (email: string): Promise<boolean> => {
-//   try {
-// const user = await client.query<GetUserQuery, GetUserQueryVariables>({
-//   query: GET_USER,
-//   variables: {
-//     whereUser: {
-//       email: { _eq: email },
-//     },
-//   },
-// });
-
-//     return !!user.data.user.length;
-//   } catch (error) {
-//     errorResponseHandler(error, "Failed to Get User");
-//     throw new Error("Failed to Get User");
-//   }
-// };
-
-export const POST = async (req: CustomNextApiRequest) => {
-  const { email }: NCheckEmail.IRequest["body"] = await req.json();
-
+const checkUserExistence = async (email: string): Promise<boolean> => {
   try {
-    await client.query<GetUserQuery, GetUserQueryVariables>({
+    const user = await client.query<GetUserQuery, GetUserQueryVariables>({
       query: GET_USER,
       variables: {
         whereUser: {
@@ -44,16 +24,25 @@ export const POST = async (req: CustomNextApiRequest) => {
       },
     });
 
+    return !!user.data.user.length;
+  } catch (error) {
+    errorResponseHandler(error, "Failed to Get User");
+    throw new Error("Failed to Get User");
+  }
+};
+
+export const POST = async (req: CustomNextApiRequest) => {
+  const { email }: NCheckEmail.IRequest["body"] = await req.json();
+
+  try {
+    const emailExist = await checkUserExistence(email);
+
     return NextResponse.json(
-      { message: "Success", response: { emailExist: true } },
+      { message: "Success", response: { emailExist: emailExist } },
       { status: StatusCodes.okStatus }
     );
   } catch (error) {
-    console.error("error -> ", error);
-    return NextResponse.json(
-      { message: `Error, ${error}` },
-      { status: StatusCodes.internalServerError }
-    );
+    return errorResponseHandler(error, "Failed to get User POST");
   }
   // try {
   //   const requestData: NCheckEmail.IRequest["body"] = await req.json();
