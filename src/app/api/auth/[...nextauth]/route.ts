5a270e752406a618883f65560f9dd5784e055359
ArnasDickus <arnasdickus@protonmail.com>
2023-12-05 14:10:49 +0200

    #92 update nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,12 +3,21 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
 import client from "../../../../../apollo-client";
 import { GET_USER } from "@/store/modules/user/query";
-
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   session: {
     strategy: "jwt",
   },
   providers: [
+    GithubProvider({
+      clientId: "dd",
+      clientSecret: "dsd",
+    }),
+    GoogleProvider({
+      clientId: "dd",
+      clientSecret: "sdsd",
+    }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
